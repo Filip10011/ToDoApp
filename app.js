c45982080533ed10eae7acde810aca9935efe464
Filip Popovski <popovski_f@hotmail.com>
2023-01-10 16:52:42 +0100

    final app

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
-const todoList = document.querySelector("todo-list");
+const todoList = document.querySelector(".todo-list");
 const completed = document.querySelector(".completed");
 const uncompleted = document.querySelector(".uncompleted");
 const all = document.querySelector(".allOption");
@@ -9,6 +9,9 @@ const allTodos = [];
 
 const addToDo = (e) => {
   e.preventDefault();
+  if (todoInput.value.length <= 4) {
+    return;
+  }
 
   const todoDivEl = document.createElement("div");
   todoDivEl.classList.add("todo");
@@ -17,6 +20,7 @@ const addToDo = (e) => {
   liEl.innerHTML = todoInput.value;
 
   allTodos.push(todoInput.value);
+
   liEl.classList.add("todo-item");
   todoDivEl.appendChild(liEl);
 
@@ -31,6 +35,53 @@ const addToDo = (e) => {
   todoDivEl.appendChild(deleteButton);
 
   todoList.appendChild(todoDivEl);
+  todoInput.value = "";
+};
+
+const deleteToDo = (e) => {
+  const element = e.target;
+
+  if (element.classList[0] === "trash-btn") {
+    const todo = element.parentElement;
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", (e) => {
+      todo.remove();
+    });
+  }
+  if (element.classList[0] === "complete-btn") {
+    const todo = element.parentElement;
+    todo.classList.toggle("completed");
+  }
+};
+
+const filterTodo = (event) => {
+  const todos = todoList.childNodes;
+
+  todos.forEach((todo) => {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 };
 
 todoButton.addEventListener("click", addToDo);
+todoList.addEventListener("click", deleteToDo);
+completed.addEventListener("click", filterTodo);
+uncompleted.addEventListener("click", filterTodo);
+all.addEventListener("click", filterTodo);
