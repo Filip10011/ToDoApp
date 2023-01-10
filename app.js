be802ce16e390d39577ec3caea84c1a23f3315ab
Filip Popovski <popovski_f@hotmail.com>
2023-01-10 13:48:02 +0100

    basic things

---
+++ app.js
@@ -0,0 +1,9 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector("todo-list");
+
+const completed = document.querySelector(".completed");
+const uncompleted = document.querySelector(".uncompleted");
+const all = document.querySelector(".allOption");
+
+const allTodos = [];
